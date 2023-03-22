f7e7df54fd788e87973e5a09bb0951ee9db1fd7d
iamAbhishekkumar <abhisonuk123@gmail.com>
2023-03-22 22:43:13 +0530

    changed from xml based config to annotation based config

--- first_project/src/main/java/com/abhishek/Car.java
+++ first_project/src/main/java/com/abhishek/Car.java
@@ -1,5 +1,8 @@
 package com.abhishek;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car implements Vehicle {
     public void drive() {
         System.out.println("Its running....");

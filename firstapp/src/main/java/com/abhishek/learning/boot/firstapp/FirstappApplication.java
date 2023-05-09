2b568a9ac9987d10c09c7092b7212788436e0fa9
iamAbhishekkumar <abhisonuk123@gmail.com>
2023-05-09 18:54:50 +0530

    created a spring web app - kinda flask

---
+++ firstapp/src/main/java/com/abhishek/learning/boot/firstapp/FirstappApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.learning.boot.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+}
